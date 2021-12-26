d3a3fc2c01b2d1496ff51c69f3b1f292bad36c02
williams104 <williams.gaming.eternal@gmail.com>
2021-12-26 13:09:16 -0500

    Update VuforiaLocalizerWrapper.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaLocalizerWrapper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaLocalizerWrapper.java
@@ -130,7 +130,7 @@ public class VuforiaLocalizerWrapper implements Localizer {
                 // express position (translation) of robot in inches.
                 VectorF translation = lastLocation.getTranslation();
                 //telemetry.addData("Pos (inches)", "{X, Y, Z} = %.1f, %.1f, %.1f",
-                        translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
+                //        translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
 
                 // express the rotation of the robot in degrees.
                 Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
@@ -165,7 +165,7 @@ public class VuforiaLocalizerWrapper implements Localizer {
                 // express position (translation) of robot in inches.
                 VectorF translation = lastLocation.getTranslation();
                 //telemetry.addData("Pos (inches)", "{X, Y, Z} = %.1f, %.1f, %.1f",
-                        translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
+                //        translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
 
                 // express the rotation of the robot in degrees.
                 Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
