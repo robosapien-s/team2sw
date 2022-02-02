d167ff557142764d755dc4b2baee1bcd5b8d4e24
KabbyDabby <kabirgoyal@icloud.com>
2022-02-01 21:34:33 -0500

    Create Checkup.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/competitionopmodes/Checkup.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.competitionopmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.wrappers.ArmWrapper;
+
+@Autonomous
+public class Checkup extends OpMode {
+    @Override
+    public void init() {
+        ArmWrapper armWrapper;
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
