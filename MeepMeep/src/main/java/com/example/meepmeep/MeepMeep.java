d6403823aa809919a07250f4a808e284925f60c0
williams104 <williams.gaming.eternal@gmail.com>
2022-02-20 22:43:05 -0500

    Drew out autonomous

--- MeepMeep/src/main/java/com/example/meepmeep/MeepMeep.java
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeep.java
@@ -8,10 +8,33 @@ import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 class MeepMeepTesting {
+
+    final static int IndexStart = 0;
+    final static int IndexDrop1 = 1;
+    final static int IndexOutsideWH = 2;
+    final static int IndexInsideWH = 3;
+    final static int IndexOutsideWH2 = 4;
+    final static int IndexDrop2 = 5;
+
+
     public static void main(String[] args) {
         // Declare a MeepMeep instance
         // With a field size of 800 pixels
         MeepMeep meepMeep = new MeepMeep(800);
+
+
+        Pose2d[] poseLocationsRed = new Pose2d[] {
+                new Pose2d(10, -60, Math.toRadians(90)),
+                new Pose2d(5,-38,Math.toRadians(130)),
+                new Pose2d(10,-60,0),
+                new Pose2d(45,-60,0),
+                new Pose2d(20,-60, 0),
+                new Pose2d(-6,-39, Math.toRadians(-250))
+
+        };
+
+        //Float[] locations = new Pose2d
+        //Float[] rotations;
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Required: Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
@@ -19,13 +42,49 @@ class MeepMeepTesting {
                 // Option: Set theme. Default = ColorSchemeRedDark()
                 .setColorScheme(new ColorSchemeRedDark())
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(10, -60, Math.toRadians(90)))
-                                .splineTo(new Vector2d(5,-38),Math.toRadians(120))
-                                .lineToLinearHeading(new Pose2d(10,-60,0))
-                                .lineTo(new Vector2d(45,-60))
-                                .lineTo(new Vector2d(20,-60))
-                                .splineTo(new Vector2d(0,-37.5), Math.toRadians(-240))
+                        drive.trajectorySequenceBuilder(poseLocationsRed[IndexStart])
+
+
+
+                                .splineTo(new Vector2d(poseLocationsRed[IndexDrop1].getX(),poseLocationsRed[IndexDrop1].getY()),poseLocationsRed[IndexDrop1].getHeading()) //1
+                                .lineToLinearHeading(poseLocationsRed[IndexOutsideWH]) //2
+                                .lineTo(new Vector2d(poseLocationsRed[IndexInsideWH].getX(),poseLocationsRed[IndexInsideWH].getY())) //3
+                                .lineTo(new Vector2d(poseLocationsRed[IndexOutsideWH2].getX(),poseLocationsRed[IndexOutsideWH2].getY())) //4
+                                .splineTo(new Vector2d(poseLocationsRed[IndexDrop2].getX(),poseLocationsRed[IndexDrop2].getY()), poseLocationsRed[IndexDrop2].getHeading()) //5
+                                .waitSeconds(.1)
+                                .splineTo(new Vector2d(poseLocationsRed[IndexOutsideWH2].getX(),poseLocationsRed[IndexOutsideWH2].getY()),poseLocationsRed[IndexOutsideWH2].getHeading()) //4
+                                .lineTo(new Vector2d(poseLocationsRed[IndexInsideWH].getX(),poseLocationsRed[IndexInsideWH].getY())) //3
+
+
+                                .lineTo(new Vector2d(poseLocationsRed[IndexOutsideWH2].getX(),poseLocationsRed[IndexOutsideWH2].getY())) //4
+                                .splineTo(new Vector2d(poseLocationsRed[IndexDrop2].getX(),poseLocationsRed[IndexDrop2].getY()), poseLocationsRed[IndexDrop2].getHeading()) //5
+                                .waitSeconds(.1)
+                                .splineTo(new Vector2d(poseLocationsRed[IndexOutsideWH2].getX(),poseLocationsRed[IndexOutsideWH2].getY()),poseLocationsRed[IndexOutsideWH2].getHeading()) //4
+                                .lineTo(new Vector2d(poseLocationsRed[IndexInsideWH].getX(),poseLocationsRed[IndexInsideWH].getY())) //3
+
+                                .lineTo(new Vector2d(poseLocationsRed[IndexOutsideWH2].getX(),poseLocationsRed[IndexOutsideWH2].getY())) //4
+                                .splineTo(new Vector2d(poseLocationsRed[IndexDrop2].getX(),poseLocationsRed[IndexDrop2].getY()), poseLocationsRed[IndexDrop2].getHeading()) //5
+                                .waitSeconds(.1)
+                                .splineTo(new Vector2d(poseLocationsRed[IndexOutsideWH2].getX(),poseLocationsRed[IndexOutsideWH2].getY()),poseLocationsRed[IndexOutsideWH2].getHeading()) //4
+                                .lineTo(new Vector2d(poseLocationsRed[IndexInsideWH].getX(),poseLocationsRed[IndexInsideWH].getY())) //3
+
+
+                                .lineTo(new Vector2d(poseLocationsRed[IndexOutsideWH2].getX(),poseLocationsRed[IndexOutsideWH2].getY())) //4
+                                .splineTo(new Vector2d(poseLocationsRed[IndexDrop2].getX(),poseLocationsRed[IndexDrop2].getY()), poseLocationsRed[IndexDrop2].getHeading()) //5
+                                .waitSeconds(.1)
+                                .splineTo(new Vector2d(poseLocationsRed[IndexOutsideWH2].getX(),poseLocationsRed[IndexOutsideWH2].getY()),poseLocationsRed[IndexOutsideWH2].getHeading()) //4
+                                .lineTo(new Vector2d(poseLocationsRed[IndexInsideWH].getX(),poseLocationsRed[IndexInsideWH].getY())) //3
+
+
+                                .lineTo(new Vector2d(poseLocationsRed[IndexOutsideWH2].getX(),poseLocationsRed[IndexOutsideWH2].getY())) //4
+                                .splineTo(new Vector2d(poseLocationsRed[IndexDrop2].getX(),poseLocationsRed[IndexDrop2].getY()), poseLocationsRed[IndexDrop2].getHeading()) //5
+                                .waitSeconds(.1)
+                                .splineTo(new Vector2d(poseLocationsRed[IndexOutsideWH2].getX(),poseLocationsRed[IndexOutsideWH2].getY()),poseLocationsRed[IndexOutsideWH2].getHeading()) //4
+                                .lineTo(new Vector2d(poseLocationsRed[IndexInsideWH].getX(),poseLocationsRed[IndexInsideWH].getY())) //3
+
                                 .build()
+
+
                 );
 
         // Set field image
