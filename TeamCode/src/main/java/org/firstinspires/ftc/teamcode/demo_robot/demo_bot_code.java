afb9b30cd3da4bc210195b92c952d8accc5ca7e4
KabbyDabby <kabirgoyal@icloud.com>
2022-04-30 15:13:48 -0400

    Create demo_bot_code.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/demo_robot/demo_bot_code.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.demo_robot;
+
+import org.firstinspires.ftc.teamcode.wrappers.DrivingWrapper;
+import org.firstinspires.ftc.teamcode.wrappers.JoystickWrapper;
+
+public class demo_bot_code{
+
+    JoystickWrapper joystickWrapper;
+    DrivingWrapper drivingWrapper;
+
+
+
+
+    drivingWrapper.Drive(joystickWrapper, speed, rotSpeed); //Driving code. See DrivingWrapper
+
+}
