eb17e2217df166365e0458794c99e86399895d41
williams104 <williams.gaming.eternal@gmail.com>
2022-11-19 12:31:05 -0500

    quikmeths

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/QuikMaths.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/QuikMaths.java
@@ -5,8 +5,24 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 public class QuikMaths {
 
-    public double RadToDeg = 57.2957795;
-    public double DegToRad = 0.01745329;
+    public double RadToDeg = 57.2957795,
+    DegToRad = 0.01745329,
+    FtToM = 0.3048,
+    MToFt = 3.2808399,
+    MiToFt = 5280,
+    FtToMi = 0.00018939,
+    InToFt = 0.08333333,
+    FtToIn = 12;
+
+    public float Clamp(float min, float max, float n){
+        if (n>max){
+            return max;
+        }
+        if (n<min){
+            return min;
+        }
+        return n;
+    }
 
     public double Atan2(float x, float y){
         return Math.atan2(y,x);
@@ -24,10 +40,18 @@ public class QuikMaths {
     public double GetAngleFromVector(float x, float y){
        return Atan2(x, y) * RadToDeg;
     }
+    public double GetAngleFromVector(double x, double y){
+        return Atan2(x, y) * RadToDeg;
+    }
     public double GetAngleFromVector(Vector2d vector2d){
         return Atan2(vector2d) * RadToDeg;
     }
     public double GetAngleFromVector(Pose2d pose2d){
         return Atan2(pose2d) * RadToDeg;
     }
+
+    public void QuikMeths(){
+        QuikMeths();
+    }
+
 }
