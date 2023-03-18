229d1f6e740671b1d1c2f5b80a354aa72e7169f1
Kabir <kabirgoyal@icloud.com>
2023-03-18 10:48:51 -0400

    ConeDetectionStuff
    Currently does not compile, and i think i got it as far as I can through chatGPT. At this point it no longer understands what im asking, so we will edit manually from here.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/wrappers/ConeDetectionWrapper.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.wrappers;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+public class ConeDetectionWrapper {
+    private final DrivingWrapperPara drivingWrapper;
+    private final HardwareMap hardwareMap;
+    private final Telemetry telemetry;
+    private final Mat workingImage;
+
+    private final Scalar lowerBound;
+    private final Scalar upperBound;
+
+    private final double DRIVE_POWER = 0.5;
+    private final double ROTATE_POWER = 0.3;
+
+    public ConeDetectionWrapper(HardwareMap hardwareMap, Telemetry telemetry) {
+        this.hardwareMap = hardwareMap;
+        this.telemetry = telemetry;
+        this.drivingWrapper = new DrivingWrapperPara(hardwareMap, telemetry);
+        this.workingImage = new Mat();
+
+        this.lowerBound = new Scalar(10, 100, 100);
+        this.upperBound = new Scalar(30, 255, 255);
+    }
+
+    public void coneDetectionLoop() {
+        while (!Thread.currentThread().isInterrupted()) {
+            Mat input = drivingWrapper.getWorkingImage();
+
+            Imgproc.cvtColor(input, workingImage, Imgproc.COLOR_RGB2HSV);
+
+            Mat mask = new Mat();
+            Core.inRange(workingImage, lowerBound, upperBound, mask);
+
+            // Detect cones and drive towards them
+            // ...
+
+            // Drive the robot
+            drivingWrapper.Drive(joystickWrapper, DRIVE_POWER, ROTATE_POWER);
+        }
+    }
+
+    public void stop() {
+        drivingWrapper.stop();
+    }
+}
