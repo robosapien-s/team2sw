0aade48bb73454fe88662645dc90779e51dc6fec
williams104 <67794864+williams104@users.noreply.github.com>
2023-04-09 19:16:45 -0400

    aaaayyayayay

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/wrappers/IDrivingTranslator.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.wrappers;
+
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+
+public interface IDrivingTranslator {
+    public Pose2d update(Pose2d pose2d);
+}
